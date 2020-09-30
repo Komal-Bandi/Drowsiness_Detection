e5434914707f85ce32f5a8024164b8b5b4190543
KOMAL-BANDI <komalbandi22@gmail.com>
2020-09-30 14:40:40 +0530

    Test

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -10,7 +10,7 @@ def eye_aspect_ratio(eye):
     C = dist.euclidean(eye[0], eye[3])
     ear = (A + B) / (2.0 * C)
     return ear
-ear_thresh = 0.23
+ear_thresh = 0.21
 frames_thresh = 48
 detect = dlib.get_frontal_face_detector()
 predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -32,19 +32,20 @@ while True:
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
         ear = (leftEAR + rightEAR) / 2.0
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        #leftEyeHull = cv2.convexHull(leftEye)
+        #rightEyeHull = cv2.convexHull(rightEye)
+        #cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        #cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
         if ear < ear_thresh:
             count += 1
             print (count)
-            if count >= frames_thresh:
-                cv2.imwrite("Dataset/Drowsy(%f).jpg"%ear,frame)                
+            cv2.imwrite("Drowsy_Dataset/Drowsy(%f).jpg"%ear,frame)
+            if count >= frames_thresh:                
                 cv2.putText(frame, "DROWSINESS ALERT!", (125, 300),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         else:
             count = 0
+            cv2.imwrite("Non_Drowsy_Dataset/Non_Drowsy(%f).jpg"%ear,frame)
         cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0,0), 2)
     end_time = time.time() 
