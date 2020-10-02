84b434b0d3800f79c4975dd44b37afa4a163c609
KOMAL-BANDI <komalbandi22@gmail.com>
2020-10-02 13:02:40 +0530

    Alarm

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -4,12 +4,16 @@ import imutils
 import dlib
 import cv2
 import time
+import pyaudio  
+import wave
+
 def eye_aspect_ratio(eye):
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
     C = dist.euclidean(eye[0], eye[3])
     ear = (A + B) / (2.0 * C)
     return ear
+
 ear_thresh = 0.21
 frames_thresh = 48
 detect = dlib.get_frontal_face_detector()
@@ -18,8 +22,16 @@ predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 cap=cv2.VideoCapture(0)
 count=0
+chunk=1024
+
 while True:
     start_time = time.time()
+    f = wave.open(r"alarm.wav","rb")
+    p = pyaudio.PyAudio()            
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+    channels = f.getnchannels(),  
+    rate = f.getframerate(),  
+    output = True)
     ret, frame=cap.read()
     frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -43,8 +55,12 @@ while True:
             if count >= frames_thresh:                
                 cv2.putText(frame, "DROWSINESS ALERT!", (125, 300),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                data = f.readframes(chunk)                   
+                stream.write(data)            
         else:
             count = 0
+            stream.stop_stream()  
+            stream.close()           
             cv2.imwrite("Non_Drowsy_Dataset/Non_Drowsy(%f).jpg"%ear,frame)
         cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0,0), 2)
@@ -56,7 +72,8 @@ while True:
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         cv2.destroyAllWindows()
-        cap.release()      
+        cap.release()
+        p.terminate()      
         break
 
 
